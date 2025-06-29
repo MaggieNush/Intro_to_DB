0cbde966d8057d0bf5b169b4b2f04b9a2b731812
MaggieNush <maggienungari.mn@gmail.com>
2025-06-29 13:36:14 +0300

    Add new sql file

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,8 +1,8 @@
-CREATE DATABASE alx_book_store;
+CREATE DATABASE IF NOT EXISTS alx_book_store;
 
 USE DATABASE alx_book_store;
 
-CREATE TABLE Books(
+CREATE TABLE IF NOT EXISTS Books(
     book_id PRIMARY KEY,
     title VARCHAR (130),
     author_id,
@@ -12,19 +12,19 @@ CREATE TABLE Books(
     FOREIGN KEY (author_id) REFERENCES Authors(author_id)
 );
 
-CREATE TABLE Authors(
+CREATE TABLE IF NOT EXISTS Authors(
     author_id PRIMARY KEY,
     author_name VARCHAR(215)
 );
 
-CREATE TABLE Customers(
+CREATE TABLE IF NOT EXISTS Customers(
     customer_id PRIMARY KEY,
     customer_name VARCHAR(215),
     email VARCHAR(215),
     address TEXT
 );
 
-CREATE TABLE Orders(
+CREATE TABLE IF NOT EXISTS Orders(
     order_id PRIMARY KEY,
     customer_id,
     order_date DATE,
@@ -32,7 +32,7 @@ CREATE TABLE Orders(
     FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
-CREATE TABLE Order_Details(
+CREATE TABLE IF NOT EXISTS Order_Details(
     orderdetailid PRIMARY KEY,
     order_id,
     book_id,
