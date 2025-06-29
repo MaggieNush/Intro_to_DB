df2d1019973566f9d4b9baaad1c0a30a51397338
MaggieNush <maggienungari.mn@gmail.com>
2025-06-29 13:33:55 +0300

    Add new sql file

---
+++ alx_book_store.sql
@@ -0,0 +1,41 @@
+CREATE DATABASE alx_book_store;
+
+USE DATABASE alx_book_store;
+
+CREATE TABLE Books(
+    book_id PRIMARY KEY,
+    title VARCHAR (130),
+    author_id,
+    price DOUBLE,
+    publication_date DATE,
+
+    FOREIGN KEY (author_id) REFERENCES Authors(author_id)
+);
+
+CREATE TABLE Authors(
+    author_id PRIMARY KEY,
+    author_name VARCHAR(215)
+);
+
+CREATE TABLE Customers(
+    customer_id PRIMARY KEY,
+    customer_name VARCHAR(215),
+    email VARCHAR(215),
+    address TEXT
+);
+
+CREATE TABLE Orders(
+    order_id PRIMARY KEY,
+    customer_id,
+    order_date DATE,
+
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+);
+
+CREATE TABLE Order_Details(
+    orderdetailid PRIMARY KEY,
+    order_id,
+    book_id,
+    quantity DOUBLE
+);
+
